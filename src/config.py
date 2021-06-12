ee1c06490ebb93b10c0618e9321e07c6dad33615
Hazal <hazalturkmen91@gmail.com>
2021-06-12 21:09:13 +0300

    Initial commit

---
+++ src/config.py
@@ -0,0 +1,20 @@
+import json
+from bunch import Bunch
+import os
+
+
+def get_config_from_json(json_file):
+    # parse the configurations from the config json file provided
+    with open(json_file, 'r') as config_file:
+        config_dict = json.load(config_file)
+
+    config = Bunch(config_dict)
+
+    return config, config_dict
+
+
+def process_config(json_file):
+    config, _ = get_config_from_json(json_file)
+    config.summary_dir = os.path.join("../experiments", config.exp_name, "summary/")
+    config.checkpoint_dir = os.path.join("../experiments", config.exp_name, "checkpoint/")
+    return config
