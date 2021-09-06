15d4705a4dff5ea11a19c8ed4b2dda07ed3ab262
hazalturkmen <hazalturkmen91@gmail.com>
2021-09-06 10:39:10 +0300

    refactored code

--- pytorch/src/bert_layer.py
+++ pytorch/src/bert_layer.py
@@ -5,12 +5,17 @@ from transformers import BertModel
 class BERTClass(torch.nn.Module):
     def __init__(self):
         super(BERTClass, self).__init__()
-        self.l1 = BertModel.from_pretrained('bert-base-uncased')
+        self.l1 = BertModel.from_pretrained("dbmdz/bert-base-turkish-cased")
         self.l2 = torch.nn.Dropout(0.3)
         self.l3 = torch.nn.Linear(768, 3)
 
-    def forward(self, ids, mask, token_type_ids):
-        _, output_1 = self.l1(ids, attention_mask=mask, token_type_ids=token_type_ids)
+    def forward(self, input_ids, attention_mask):
+        _, output_1 = self.l1(input_ids=input_ids, attention_mask=attention_mask, return_dict=False)
         output_2 = self.l2(output_1)
         output = self.l3(output_2)
         return output
+
+
+
+
+
