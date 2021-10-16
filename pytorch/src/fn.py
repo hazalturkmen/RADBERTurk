5ce6e43b4782e04f6250af7d5f2799bc32e1ce6a
hazalturkmen <hazalturkmen91@gmail.com>
2021-10-16 14:09:12 +0300

    change project directory

---
+++ pytorch/src/fn.py
@@ -0,0 +1,5 @@
+import os
+
+from transformers import BertModel
+for i in range(15, 73):
+    os.mkdir(r'C:\Users\AI User\Desktop\TurkRADBERT\pytorch\src\outputs\output'+str(i))
--- pytorch/src/preproc.py
+++ pytorch/src/preproc.py
@@ -10,7 +10,7 @@ def preproc(sentence):
     :return:sentece
     """
     sentence = sentence.strip()
-    #sentence = sentence.lower()
+    sentence = sentence.lower()
     punc = '''!()-[]{};:'"\,<>./?@$%^&±*_~'''
     sentence = re.sub("[0-9]", "#", sentence)
     for ele in sentence:
@@ -20,9 +20,9 @@ def preproc(sentence):
 
 
 def preproc_files():
-    files = glob.glob("/home/hazal/nlp_dataset/brain_CT/splitted_txt_2/*.txt")
+    files = glob.glob("/home/hazal/nlp_dataset/brain_CT/splitted_txt_processed_2/*.txt")
     print("\nPre-processing medical text...")
-    with open('/home/hazal/nlp_dataset/brain_CT/tr_medical_processed', 'w') as fo:
+    with open('/home/hazal/nlp_dataset/brain_CT/tr_articles_processed_lower_2', 'w') as fo:
         for f in tqdm(files):
             f_txt = open(f, "r")
             file_contents = f_txt.read()
@@ -35,8 +35,8 @@ def preproc_files():
 
 def preproc_single_file():
     print("\nPre-processing medical text...")
-    with open('/home/hazal/nlp_dataset/brain_CT/tr_medical_data/tr_articles_processed.txt', 'w') as fo:
-        f_txt = open("/home/hazal/nlp_dataset/brain_CT/tr_medical_data/tr_articles_2.txt", "r")
+    with open('/home/hazal/nlp_dataset/brain_CT/tr_thesis_processed.txt', 'w') as fo:
+        f_txt = open("/home/hazal/nlp_dataset/brain_CT/tr_thesis_1.txt", "r")
         file_contents = f_txt.read()
         contents_split = file_contents.splitlines()
         for sent in contents_split:
@@ -48,4 +48,4 @@ def preproc_single_file():
 
 
 if __name__ == '__main__':
-    preproc_single_file()
+    preproc_files()
