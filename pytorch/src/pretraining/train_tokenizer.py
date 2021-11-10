a3cada5f18eded7a051b9425070c0a7eb2ebb4cc
hazalturkmen <hazalturkmen91@gmail.com>
2021-11-10 18:14:27 +0300

    refactored code

--- pytorch/src/pretraining/train_tokenizer.py
+++ pytorch/src/pretraining/train_tokenizer.py
@@ -1,14 +1,17 @@
 from tokenizers import BertWordPieceTokenizer
 
-tokenizer_cased = BertWordPieceTokenizer(
+tokenizer = BertWordPieceTokenizer(
     clean_text=True,
     handle_chinese_chars=False,
     strip_accents=False,
     lowercase=False,
 )
 
-trainer_cased = tokenizer_cased.train(
-    "/home/hazal/nlp_dataset/corpus/all.txt",
+print("Training is started...")
+
+
+trainer = tokenizer.train(
+    r"C:\Users\AI User\Desktop\TurkRADBERT\corpus\all.txt",
     vocab_size=32000,
     min_frequency=2,
     show_progress=True,
@@ -16,25 +19,6 @@ trainer_cased = tokenizer_cased.train(
     limit_alphabet=1000,
     wordpieces_prefix="##"
 )
+print("Training is over...")
 
-tokenizer_cased.save("./", "cased")
-
-
-tokenizer_uncased = BertWordPieceTokenizer(
-    clean_text=True,
-    handle_chinese_chars=False,
-    strip_accents=False,
-    lowercase=True,
-)
-
-trainer_uncased = tokenizer_uncased.train(
-    "/home/hazal/nlp_dataset/corpus/all.txt",
-    vocab_size=32000,
-    min_frequency=2,
-    show_progress=True,
-    special_tokens=['[PAD]', '[UNK]', '[CLS]', '[SEP]', '[MASK]'],
-    limit_alphabet=1000,
-    wordpieces_prefix="##"
-)
-
-tokenizer_uncased.save("./", "uncased")
+tokenizer.save("./", "cased")
